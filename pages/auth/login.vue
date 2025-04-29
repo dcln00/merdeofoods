aa66f4b63fceed3e3d872109289709ee27f2bc04
Nii Aryeh <nii.aryh@gmail.com>
2025-04-29 02:30:26 +0000

    login update

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -18,9 +18,10 @@ const logiin = async () => {}
 </script>
 
 <template lang="pug">
-section#login(class="h-screen flex items-center")
-	.container(class="grid grid-cols-2 gap-2")
-		div hello
-		div
-			ui-form(heading="Welcome back!" tagline="Log in to your account by completing the form below" :input="input" @handle-submit="login" @google-auth="signInWithOAuth")
+section#login(class="py-0 lg:py-20")
+	div(class="h-screen flex items-center")
+		.container(class="grid grid-cols-2 gap-2")
+			div(class="hidden lg:block") hello
+			div(class="col-span-full lg:col-span-1")
+				ui-form(heading="Welcome back!" tagline="Log in to your account by completing the form below" :input="input" @handle-submit="login" @google-auth="signInWithOAuth")
 </template>
