df641c5d69b2c4d3a5bb5d35e9b7b9c29a744216
Nii Aryeh <nii.aryh@gmail.com>
2025-05-01 01:32:08 +0000

    navigation button

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -12,11 +12,14 @@ div
 		.overlay(v-if="show" @click="$emit('toggleShow')" class="fixed top-0 left-0 z-10 w-full h-dvh bg-black/70")
 	Transition(name="nav")
 		nav(class="fixed top-0 right-0 z-50 bg-white w-2/3 h-screen p-8" v-if="show")
-			div(class="mb-16")
+			div(class="mb-8")
 				.nav-header(class="flex items-center mb-12")
 					svgo-close(class="text-brand-green ms-auto cursor-pointer text-xl" @click="$emit('toggleShow')")
 				ul(class="*:tracking-tight *:capitalize space-y-4")
 					li(v-for="(item, index) in nav" :key="index") #[NuxtLink(:to="item.slug" activeClass='active-link' @click="$emit('toggleShow')") {{ item.display }}]
+			div
+				NuxtLink(to="/auth/register")
+					button(class="bg-brand-green hover:bg-[#155e15] duration-300 text-white px-4 py-2 rounded w-full") Get Started
 </template>
 
 <style scoped>
