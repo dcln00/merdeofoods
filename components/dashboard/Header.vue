7f827914aee31ecd769d94c1c0f8805814f00c8a
Nii Aryeh <nii.aryh@gmail.com>
2025-05-11 18:23:24 +0000

    exact active class

--- components/dashboard/Header.vue
+++ components/dashboard/Header.vue
@@ -5,9 +5,9 @@ header(class="hidden lg:block")
 	.sub-header(class="bg-neutral-800")
 		.container
 			ul(class="flex gap-4 text-white")
-				li #[NuxtLink(to="/dashboard/commerce" activeClass='active-link' class="block py-2 px-4") Commerce]
-				li #[NuxtLink(to="/dashboard/inventory" activeClass='active-link' class="block py-2 px-4") Inventory]
-				li #[NuxtLink(to="/dashboard" activeClass='active-link' class="block py-2 px-4") Dashboard]
+				li #[NuxtLink(to="/dashboard/commerce" exactActiveClass='active-link' class="block py-2 px-4") Commerce]
+				li #[NuxtLink(to="/dashboard/inventory" exactActiveClass='active-link' class="block py-2 px-4") Inventory]
+				li #[NuxtLink(to="/dashboard" exactActiveClass='active-link' class="block py-2 px-4") Dashboard]
 				//- li(class="bg-zinc-200 text-zinc-800") Dashboard
 	.header(class="bg-zinc-200 py-4")
 		.container(class="flex items-center")
